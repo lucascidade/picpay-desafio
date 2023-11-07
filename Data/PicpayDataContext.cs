fdc060ec484235ab3d7945a375260bfb25932582
lucascidade <lucascidade96@gmail.com>
2023-11-07 12:01:32 -0400

    instalação de pacotes e criacao de construtor do User

---
+++ Data/PicpayDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace picpay_desafio.Data
+{
+    public class PicpayDataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source ./Data/picpay_desafio.db");
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -4,14 +4,29 @@ namespace picpay_desafio.Models
 {
     public class User
     {
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Wallet {  get; set; }
-        public string Document { get; set; }
-        public UserType Type { get; set; }
-        public bool Active { get; set; }
+        public User (
+            string firstName,
+            string lastName,
+            string document,
+            decimal wallet,
+            UserType type,
+            Guid Id = default) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Wallet = wallet;
+            Type = type;
+            Active = true;
+        }
 
+        public Guid Id { get;  set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public decimal Wallet {  get; private set; }
+        public string Document { get; private set; }
+        public UserType Type { get; private set; }
+        public bool Active { get; private set; }
             
     }
 }
