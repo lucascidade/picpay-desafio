b8f4435066f3ca38133350e72dce8313f4c4f717
lucascidade <lucascidade96@gmail.com>
2023-11-07 11:48:46 -0400

    Implementando Repository e Interface

---
+++ Interface/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using picpay_desafio.Models;
+
+namespace picpay_desafio.Interface.Repositories
+{
+    public interface IUserRepository
+    {
+        public Task<List<User>> GetAll();
+        public Task<User> GetById();
+        public void UpdateUser();
+        public Guid Create();
+        public Task DeleteUser(Guid id);
+
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using picpay_desafio.Interface.Repositories;
+using picpay_desafio.Models;
+
+namespace picpay_desafio.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Guid Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
